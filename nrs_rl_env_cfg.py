67759e365fd2ac35a0b0eeb4aca8916d7b62710f
eunseopsong <lexondms1@g.skku.edu>
2026-03-09 17:35:59 +0900

    hand_g_recording -> concave_g_recording

--- nrs_rl_env_cfg.py
+++ nrs_rl_env_cfg.py
@@ -238,7 +238,7 @@ class EventCfg:
         mode="reset",
         params={
             # "file_path": "/home/eunseop/nrs_lab2/datasets/hand_g_recording.h5",
-            "file_path": "/home/eunseop/nrs_rl/source/nrs_rl/nrs_rl/tasks/manager_based/nrs_rl/datasets/hand_g_recording.h5",
+            "file_path": "/home/eunseop/nrs_rl/source/nrs_rl/nrs_rl/tasks/manager_based/nrs_rl/datasets/concave_g_recording.h5",
             "dataset_key": "target_positions",
         },
     )
