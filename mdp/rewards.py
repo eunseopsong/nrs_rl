d5382e5c40197acfe175cd9c8247faea527d3159
eunseopsong <lexondms1@g.skku.edu>
2026-03-09 14:50:39 +0900

    shangus

--- mdp/rewards.py
+++ mdp/rewards.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
+# shangus
 """
 v28: Adaptive Polishing Intelligence (Position + Force + Smoothness)
 ----------------------------------------------------------------------------------
